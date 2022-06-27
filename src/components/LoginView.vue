d9b601fe5d597d3af6e326a9e4a1ecdb3aa70f7b
farahashim <farahashim99@gmail.com>
2022-06-28 04:16:09 +0800

    update

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -3,19 +3,19 @@
     <div class="login">
     <form>
   <!-- Email input -->
-  <div class="form-outline mb-4">
+  <div class="form-outline mb-2">
     <input type="email" id="form2Example1" class="form-control" />
     <label class="form-label" for="form2Example1">Email address</label>
   </div>
 
   <!-- Password input -->
-  <div class="form-outline mb-4">
+  <div class="form-outline mb-2">
     <input type="password" id="form2Example2" class="form-control" />
     <label class="form-label" for="form2Example2">Password</label>
   </div>
 
   <!-- 2 column grid layout for inline styling -->
-  <div class="row mb-4">
+  <div class="row mb-2">
     <div class="col d-flex justify-content-center">
       <!-- Checkbox -->
       <div class="form-check">
@@ -36,22 +36,6 @@
   <!-- Register buttons -->
   <div class="text-center">
     <p>Not a member? <a href="#!">Register</a></p>
-    <p>or sign up with:</p>
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-google"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-twitter"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-github"></i>
-    </button>
   </div>
 </form>
   </div>
